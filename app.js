3e5beea08b96c05a05957e928edf99af714efb67
joandiazestigarribia <68211077+joandiazestigarribia@users.noreply.github.com>
2020-07-13 00:07:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ function getTasks() { /* consulta los datos del localstorage y los muestra por p
         <div class="card-body">
             <p>${title} - ${description}</p> 
             <a class="btn btn-danger" onclick="deleteTask('${title}')">
-                Delete
+                Eliminar tarea
             </a>
             </div>
         </div>`
